a74ed8e1f7eb231ec5dc5eb949af5f0c7ac7b0fa
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-30 00:00:19 +0430

    Update controller and add new stuff

--- app/controllers/BlogControllers.js
+++ app/controllers/BlogControllers.js
@@ -10,10 +10,57 @@ const all = (req, res) => {
             }
 
             res.status(200);
+            res.send(blogs);
+        })
+        .catch((error) => {
+            res.status(500);
+            res.send(error);
+        });
+}
+
+const add = (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    const blogData = req.body;
+
+    const blog = new Blog(blogData);
+    blog.save()
+        .then((blogResult) => {
+            const data = {
+                id: blogResult._id,
+            }
+
+            res.status(200);
             res.send(data);
         })
+        .catch((error) => {
+            res.status(500);
+            res.send(error);
+        });
+}
+
+const get = (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+
+    const blog_id = req.params.blog_id;
+
+    Blog.findById(blog_id)
+        .then((blog) => {
+            const data = {
+                blog,
+            }
+
+            res.status(200);
+            res.send(data);
+        })
+        .catch((error) => {
+            res.status(500);
+            res.send(error);
+        });
 }
 
 module.exports = {
     all,
+    add,
+    get,
 }
