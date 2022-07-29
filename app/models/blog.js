dffb2b5559d422424252e6e86ec82cbee55a8776
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-29 12:23:44 +0430

    Add blog mode

---
+++ app/models/blog.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const blogModel = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    auther: {
+        type: String,
+        required: true,
+    }
+}, {
+    timestamps: true,
+});
+
+const Blog = mongoose.model('blog', blogModel);
+
+module.exports = Blog;
