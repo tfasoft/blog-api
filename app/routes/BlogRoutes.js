59ca45e831246d53ca340d311ac0c0a6692290c0
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-30 00:00:30 +0430

    Add new routes

--- app/routes/BlogRoutes.js
+++ app/routes/BlogRoutes.js
@@ -4,5 +4,7 @@ const controller = require("../controllers/BlogControllers");
 const router = express.Router();
 
 router.get('/all', controller.all);
+router.get('/get/:blog_id', controller.get)
+router.post('/add', controller.add);
 
 module.exports = router;
