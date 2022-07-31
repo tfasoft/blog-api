15a8dcf3d54af7e8a34b6f1df04f7172ce5fff76
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-31 15:37:19 +0430

    Add update route

--- app/routes/BlogRoutes.js
+++ app/routes/BlogRoutes.js
@@ -7,5 +7,6 @@ router.get('/all', controller.all);
 router.get('/get/:blog_id', controller.get)
 router.post('/add', controller.add);
 router.post('/delete/:blog_id', controller.del);
+router.post('/update', controller.update);
 
 module.exports = router;
