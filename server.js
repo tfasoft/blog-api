67357f7f05a319d65edc838ec48dc1441410dcd5
Amirhossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2023-04-02 17:34:12 +0330

    Some delete and and some new

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from "./app/index.js";
 
-import { appConfig } from "@/config";
+import { appConfig } from "$app/config/index.js";
 app.listen(appConfig.port, () => {
   console.log(`Connected. Running in ${appConfig.port}`);
 });
